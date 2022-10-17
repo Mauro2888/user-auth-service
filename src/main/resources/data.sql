30aec9cefee2ddd98859b587ed927de6217dcb11
Mauro C <mauro.dev88@gmail.com>
2022-10-17 17:17:49 +0200

    update project base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id,username,password,role)
+VALUES (1,'admin','$2a$12$Xk7oi3kCvTh6us4oU18Q6Oqo.lzO7Uxoh.G4FMyx0pYgGY0Ky.AdG','ADMIN');
+INSERT INTO users (id,username,password,role)
+VALUES (2,'user','$2a$12$G4w5CzUQtWwrIPwfjvzK5ufz.DHSPSi7SOWa512hBz5PRV85m3wXe','USER');
